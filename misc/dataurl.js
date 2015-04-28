9abe83266afff35af157738cce9460ff297d5b60
me <nmapsy@gmail.com>
2015-04-28 01:37:46 -0400

    Add old static html pages from misc/.

---
+++ misc/dataurl.js
@@ -0,0 +1,29 @@
+
+function toDataUrl() {
+  var fileInput = document.getElementById('selection');
+
+  // files is a FileList object (similar to NodeList)
+  var files = fileInput.files;
+
+  if (files.length === 0) {
+    alert('No file selected!');
+    return;
+  }
+
+  if (files[0].size > 100000) {
+    alert('File too big! Must be smaller than 100000 bytes.');
+    return;
+  }
+
+  var reader = new FileReader();
+  reader.onloadend = function() {
+    var resultBox = document.getElementById('resultBox');
+    resultBox.textContent = reader.result;
+    var result = document.getElementById('result');
+    result.style.display = '';
+  };
+
+  reader.readAsDataURL(files[0]);
+}
+
+document.getElementById('submit').onclick = toDataUrl;
