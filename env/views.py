b841011d02ded6035eb6bf82dfa213bafda10fe2
Nick <nmapsy@gmail.com>
2017-03-02 15:52:19 -0800

    env: Simplify urlpatterns for it and restrict to admin over HTTPS.

--- env/views.py
+++ env/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.conf import settings
+from myadmin.lib import get_admin_cookie
 import os
 
 def index(request):
-  text = ''
-  for key in sorted(request.META.keys()):
-    if key not in os.environ:
-      text += '{}:\t{}\n'.format(key, request.META[key])
+  # Only allow showing details about the server environment to the admin over HTTPS.
+  admin_cookie = get_admin_cookie(request)
+  if admin_cookie and (request.is_secure() or not settings.REQUIRE_HTTPS):
+    text = ''
+    for key in sorted(request.META.keys()):
+      if key not in os.environ:
+        text += '{}:\t{}\n'.format(key, request.META[key])
+  else:
+    text = 'Error: This page is restricted to the admin over HTTPS.'
   return HttpResponse(text, content_type='text/plain; charset=UTF-8')
--- mysite/urls.py
+++ mysite/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic.base import RedirectView
+import env.views
 
 urlpatterns = [
   # url(r'^admin/', include(admin.site.urls)),
-  url(r'^env$', include('env.urls')),
+  url(r'^env$', env.views.index),
   url(r'^env/$', RedirectView.as_view(url='/env', permanent=True)),
   url(r'^traffic/', include('traffic.urls')),
   url(r'^admin/', include('myadmin.urls')),
