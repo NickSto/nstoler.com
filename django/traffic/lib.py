3b01879a108e042beaaf4d9e786c3855345ace50
Nick <nmapsy@gmail.com>
2017-03-13 05:03:06 -0700

    django/traffic/lib.py: Break out logic for getting or creating Visitors into get_visitor().

--- django/traffic/lib.py
+++ django/traffic/lib.py
@@ -6,7 +6,9 @@ import random
 #TODO: Grab info about the IP address at the time. Stuff like the ISP, ASN, and geoip data.
 #      That stuff can change, especially as IPv4 addresses are bought and sold increasingly.
 #      Maybe run a daemon separate from Django to do it in the background, to not hold up the
-#      response.
+#      response. Possible data sources:
+#      http://ipinfo.io/developers (ASN, geiop)
+#      https://stat.ripe.net/docs/data_api (semi-official, but ASN only)
 
 ALPHABET1 = string.ascii_lowercase + string.ascii_uppercase + string.digits + '+-'
 COOKIE_EXPIRATION = 10*365*24*60*60  # 10 years
@@ -35,36 +37,7 @@ def add_visit(request, response, side_effects=None):
   headers = request.META
   ip = headers.get('REMOTE_ADDR')
   user_agent = headers.get('HTTP_USER_AGENT')
-  # Check if this visitor has already been seen.
-  try:
-    visitor = Visitor.objects.get(ip=ip, cookie1=cookies[0], user_agent=user_agent)
-  except Visitor.MultipleObjectsReturned:
-    #TODO: Determine more intelligently which visitor to use.
-    logging.warn('Multiple visitors found with ip "{}", cookie1 "{}", and user_agent "{}".'
-                  .format(ip, cookie[0], user_agent))
-    visitor = Visitor.objects.filter(ip=ip, cookie1=cookies[0], user_agent=user_agent)[0]
-  except Visitor.DoesNotExist:
-    # If we haven't seen this exact visitor before, have this User before?
-    # Identify the User by the cookie.
-    visitors = Visitor.objects.filter(cookie1=cookies[0])
-    if visitors:
-      # Take the label for the new visitor from the existing ones.
-      label = get_common_start([visitor.label for visitor in visitors])
-      user = visitors[0].user
-    else:
-      # If we've never seen a visitor with this cookie before, create a new User too.
-      label = ''
-      user = User()
-      user.save()
-    visitor = Visitor(
-      ip=ip,
-      user_agent=user_agent,
-      cookie1=cookies[0],
-      cookie2=cookies[1],
-      label=label,
-      user=user
-    )
-    visitor.save()
+  visitor = get_visitor(ip, cookies, user_agent)
   visit = Visit(
     method=request.method,
     scheme=request.scheme,
@@ -85,6 +58,57 @@ def add_visit(request, response, side_effects=None):
     if 'visit' in side_effects:
       side_effects['visit'] = visit
   return response
+
+
+def get_visitor(ip, cookies, user_agent):
+  """Get an existing Visitor or create one."""
+  if len(cookies) == 0:
+    cookie1 = None
+    cookie2 = None
+  elif len(cookies) == 1:
+    cookie1 = cookies[0]
+    logging.info('This Visitor already A matching Visitor was found! id = {}'.format(visitor.id))
+    cookie2 = None
+  elif len(cookies) >= 2:
+    cookie1 = cookies[0]
+    cookie2 = cookies[1]
+  # Check if this visitor has already been seen.
+  try:
+    visitor = Visitor.objects.get(ip=ip, cookie1=cookie1, user_agent=user_agent)
+    logging.info('This Visitor already exists (id {}).'.format(visitor.id))
+  except Visitor.MultipleObjectsReturned:
+    #TODO: Determine more intelligently which visitor to use.
+    logging.warn('Multiple visitors found with ip "{}", cookie1 "{}", and user_agent "{}".'
+                  .format(ip, cookie1, user_agent))
+    visitor = Visitor.objects.filter(ip=ip, cookie1=cookie1, user_agent=user_agent)[0]
+  except Visitor.DoesNotExist:
+    # If we haven't seen this exact visitor before, have this User before?
+    # Identify the User by the cookie.
+    visitors = Visitor.objects.filter(cookie1=cookie1)
+    if visitors:
+      # Take the label for the new visitor from the existing ones.
+      label = get_common_start([visitor.label for visitor in visitors])
+      user = visitors[0].user
+      logging.info('This Visitor does not exist, but one with the same cookie does (visitor id {}, '
+                   'user id {}).'.format(visitor.id, user.id))
+    else:
+      # If we've never seen a visitor with this cookie before, create a new User too.
+      label = ''
+      user = User()
+      user.save()
+      logging.info('This Visitor does not exist, and no Visitors with this cookie have been seen '
+                   'before. Creating a new one..')
+    visitor = Visitor(
+      ip=ip,
+      user_agent=user_agent,
+      cookie1=cookie1,
+      cookie2=cookie2,
+      label=label,
+      user=user
+    )
+    visitor.save()
+  return visitor
+
 
 def get_common_start(labels):
   """Example: get_common_start(['me and you', 'me and you at 1507', 'me and Emily']) -> 'me and'
