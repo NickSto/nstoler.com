00c202db614acc33572f0ab16637e7f5db097c7b
Nick <nmapsy@gmail.com>
2017-03-15 06:41:48 -0700

    301 redirect old /n/ and /p/ notepad urls in nginx conf instead of Django. - Solves bug where every notepad page had a submit url of /p/pagename.

--- django/mysite/urls.py
+++ django/mysite/urls.py
@@ -22,8 +22,6 @@ urlpatterns = [
   url(r'^traffic/', include('traffic.urls')),
   url(r'^admin/', include('myadmin.urls')),
   # url(r'^wikihistory/', include('wikihistory.urls')),
-  url(r'^p/', include('notepad.urls')),  # Old notepad url.
-  url(r'^n/', include('notepad.urls')),  # Same.
   # If nothing else matches, send it to notepad.
   url(r'', include('notepad.urls')),
 ]
