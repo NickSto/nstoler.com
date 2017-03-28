04657443d2e4ff6fd7f3f61cd8857df9fe583fe9
Nick <nmapsy@gmail.com>
2017-03-28 01:53:57 -0700

    1.0: Remove some obsolete code and files.

--- 1.0/js/script.js
+++
@@ -1,9 +0,0 @@
-/* Author: 
-
-*/
-
-$(document).ready( function() {
-	var linkElement = document.getElementById("nom")
-	var link = linkElement.getAttribute("href");
-	linkElement.setAttribute("href", link + "?l=2&r=" + document.referrer);
-});
