354df9e993f13b59a18a9e618ca6294424817c45
Nick <nmapsy@gmail.com>
2017-03-28 01:11:19 -0700

    traffic: Update is_robot() with new types of user agents and checking the host header.

--- django/traffic/lib.py
+++ django/traffic/lib.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from .models import Visit, Visitor, User
 import functools
 import string
@@ -251,15 +252,19 @@ def encode_cookie(uid):
 
 
 # Bot detection:
+#TODO: Put these in the database for more flexibility and possibly even a gain in speed.
 # These names occur in a known location in the user_agent string: After "compatible; " and before
 # "/" or ";", e.g.: Mozilla/5.0 (compatible; Exabot/3.0; +http://www.exabot.com/go/robot)
-UA_BOT_NAMES = ('Googlebot', 'bingbot', 'Baiduspider', 'YandexBot', 'Yahoo! Slurp', 'AhrefsBot',
+UA_BOT_NAMES = ('Googlebot', 'bingbot', 'Baiduspider', 'YandexBot', 'AhrefsBot', 'Yahoo! Slurp',
                 'Exabot', 'Uptimebot', 'MJ12bot', 'Yeti', 'SeznamBot', 'DotBot', 'spbot', 'Ezooms',
-                'BLEXBot', 'SiteExplorer', 'SISTRIX Crawler', 'SEOkicks-Robot', 'WBSearchBot',
-                'SemrushBot')
+                'BLEXBot', 'SiteExplorer', 'SEOkicks-Robot', 'WBSearchBot', 'SemrushBot',
+                'SMTBot', 'Dataprovider.com', 'SISTRIX Crawler', 'coccocbot-web')
+# These names occur after "compatible; " and before " ". It's incompatible with the above, which
+# allows spaces in the names.
+UA_BOT_NAMES_SPACE_DELIM = ('archive.org_bot',)
 # For these, the user_agent string begins with the bot name, followed by a "/".
-UA_BOT_STARTSWITH = ('Sogou web spider', 'TurnitinBot', 'Wotbox', 'SeznamBot', 'Aboundex',
-                     'msnbot-media')
+UA_BOT_STARTSWITH = ('curl', 'Sogou web spider', 'TurnitinBot', 'Wotbox', 'SeznamBot', 'Aboundex',
+                     'msnbot-media', 'masscan')
 
 
 def is_robot(visit):
@@ -267,19 +272,39 @@ def is_robot(visit):
   if not (visit.host or visit.query_str or visit.referrer or visitor.user_agent or visitor.cookie1
           or visitor.cookie2) and (visit.path == '/' or visit.path == ''):
     return True
+  elif invalid_host(visit.host):
+    return True
   else:
     return is_robot_ua(visitor.user_agent)
+
+
+def invalid_host(host):
+  """Check every level of subdomains to allow things like "www.nstoler.com" to match "nstoler.com".
+  """
+  domain = ''
+  for subdomain in reversed(host.split('.')):
+    if domain:
+      domain = subdomain + '.' + domain
+    else:
+      domain = subdomain
+    if domain in settings.ALLOWED_HOSTS:
+      return False
+  return True
 
 
 def is_robot_ua(user_agent):
   if user_agent is None or user_agent == '':
     return True
   # Does the user_agent contain a known bot name in the standard position?
-  fieldsA = user_agent.split('compatible; ')
-  if len(fieldsA) >= 2:
-    fieldsB = fieldsA[1].split('/')
-    fieldsC = fieldsB[0].split(';')
-    if fieldsC[0] in UA_BOT_NAMES:
+  ua_halves = user_agent.split('compatible; ')
+  if len(ua_halves) >= 2:
+    # Look for it after 'compatible; ' and before '/', ';', or whitespace.
+    bot_name = ua_halves[1].split('/')[0].split(';')[0]
+    if bot_name in UA_BOT_NAMES:
+      return True
+    # Check for bot names that contain whitespace (same as above, but don't split on whitespace)
+    bot_name = bot_name.split()[0]
+    if bot_name in UA_BOT_NAMES_SPACE_DELIM:
       return True
   # Does the user_agent start with a known bot name?
   fields = user_agent.split('/')
