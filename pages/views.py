a436e7c60a33a7c4c7c038ace1204578a03464a3
Nick <nmapsy@gmail.com>
2017-04-17 11:35:11 -0400

    pages: Add prototype for dynamic homepage.

--- pages/views.py
+++ pages/views.py
@@ -4,3 +4,8 @@ from traffic.lib import add_visit
 @add_visit
 def yourgenome(request):
   return render(request, 'pages/yourgenome.tmpl')
+
+
+@add_visit
+def home(request):
+  return render(request, 'pages/home.tmpl')
