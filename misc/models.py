554ae56112f37727ecffe5c0019234664c8e8f5e
Nick <nmapsy@gmail.com>
2017-10-10 01:43:35 -0700

    misc: Add TempData table for misc, non-critical info.

--- misc/models.py
+++ misc/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from utils import ModelMixin
 
-# Create your models here.
+# Minimally structured temporary storage bin.
+class TempData(ModelMixin, models.Model):
+  # A number you can use to identify the data later E.g. pass it to a background task so it can
+  # create a TempData when it's done, then find it via the identifier in the main thread.
+  identifier = models.IntegerField(null=True, blank=True)
+  type = models.CharField(max_length=127)
+  int = models.IntegerField(null=True, blank=True)
+  float = models.FloatField(null=True, blank=True)
+  data = models.TextField()
