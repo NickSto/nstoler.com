de30866032618c9f669bfc60bcd60ca07585ca2a
Nick <nmapsy@gmail.com>
2018-09-02 03:44:48 -0700

    Have nginx globally redirect trailing slashes to non-slashes. - Also, add redirect for old yourgenome.html url.

--- misc/urls.py
+++ misc/urls.py
@@ -5,17 +5,11 @@ from . import views
 
 app_name = 'misc'
 urlpatterns = [
-  url(r'^env/$', RedirectView.as_view(url='../env', permanent=True)),
   url(r'^env$', views.env, name='env'),
-  url(r'^userinfo/$', RedirectView.as_view(url='../userinfo', permanent=True)),
   url(r'^userinfo$', views.userinfo, name='userinfo'),
-  url(r'^setcookie/$', RedirectView.as_view(url='../setcookie', permanent=True)),
   url(r'^setcookie$', views.setcookie, name='setcookie'),
-  url(r'^dataurl/$', RedirectView.as_view(url='../dataurl', permanent=True)),
   url(r'^dataurl$', views.dataurl, name='dataurl'),
-  url(r'^sessionrecover/$', RedirectView.as_view(url='../sessionrecover', permanent=True)),
   url(r'^sessionrecover$', views.sessionrecover, name='sessionrecover'),
-  url(r'^export/$', RedirectView.as_view(url='../export', permanent=True)),
   url(r'^export$', views.export, name='export'),
   url(r'^captcha/(?P<name>.+)$', views.captcha, name='captcha'),
   url(r'^captchasubmit/(?P<name>.+)$', views.captchasubmit, name='captchasubmit'),
--- traffic/urls.py
+++ traffic/urls.py
@@ -6,7 +6,6 @@ app_name = 'traffic'
 urlpatterns = [
   # Redirect to special view in order to preserve query string.
   url(r'^monitor\.cgi$', views.monitor_redirect),
-  url(r'^monitor/$', views.monitor_redirect),
   url(r'^monitor$', views.monitor_redirect),
   url(r'^ip/(?P<ip>.+)$', views.view_ip, name='view_ip'),
   url(r'^ip$', views.view_ip),
